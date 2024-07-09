d09c6705fe6dddc543d386dda10f06a6af83aadc
Batsirai974 <batsirai3@protonmail.com>
2024-07-09 14:19:45 +0400

    mise a jours organisation et rajout formulaire d'inscriptions

---
+++ pages/formulaires-inscription/formulaire-inscription-majeur/inscription-majeur-page.js
@@ -0,0 +1,34 @@
+// anime champ formulaire ---------------------
+
+const inputs = document.querySelectorAll('.field-anime');
+
+for (let i = 0; i < inputs.length; i++) {
+    
+    let field = inputs[i];
+    console.log(field);
+
+    field.addEventListener('input', (e) => {
+
+        if(e.target.value != ""){
+            e.target.parentNode.classList.add('animation');
+        } else if (e.target.value == "") {
+            e.target.parentNode.classList.remove('animation');
+        }
+
+    })
+
+}
+
+
+
+// Toggle liste choix -------------------------------------
+
+const toggleListChoix = document.querySelector('.toggle-options');
+const multiSelect = document.querySelector('.multi-select');
+const boxChoix = document.querySelector('.choix-activites');
+
+
+toggleListChoix.addEventListener('click', function(){
+    multiSelect.classList.toggle('hidden');
+});
+
---
+++ pages/formulaires-inscription/pre-inscription-pages/pre-inscription_page.js
@@ -0,0 +1,28 @@
+
+
+const form = document.querySelector('.form-pre-inscription');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const adherent = document.querySelector('input[name="adherent"]:checked').value;
+  const majeur = document.querySelector('input[name="majeur"]:checked').value;
+
+  // Afficher les résultats dans la console
+  console.log('Étiez-vous adhérent à la saison dernière ?', adherent);
+  console.log('Avez-vous plus de 18 ans ?', majeur);
+
+  if(adherent === 'non' && majeur === 'non') {
+    window.location.href = '/pages/formulaires-inscription/formulaire-de-test/formulaire-test.html';
+    console.log("Affiche formulaire de test");
+  }
+  else if (adherent === 'oui' && majeur === 'non'){
+    window.location.href = '/pages/formulaires-inscription/formulaire-inscription-mineur/inscription-mineur-page.html';
+    console.log("Affiche formulaire inscription jeune");
+  }
+  else {
+    window.location.href = '/pages/formulaires-inscription/formulaire-inscription-majeur/inscription-majeur-page.html';
+    console.log("Affiche formulaire inscription adultes");
+  }
+ 
+});
