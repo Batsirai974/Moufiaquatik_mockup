cd53ef8e2cefa42e3155d247b9025fca9edafe92
Batsirai974 <batsirai3@protonmail.com>
2024-07-19 16:52:52 +0400

    ajout logo pass sport et pass loisirs dans paga accueil + ajout modale instruction cheque sur page inscription majeur

--- pages/formulaires-inscription/formulaire-inscription-majeur/inscription-majeur-page.js
+++ pages/formulaires-inscription/formulaire-inscription-majeur/inscription-majeur-page.js
@@ -5,7 +5,7 @@ const inputs = document.querySelectorAll('.field-anime');
 for (let i = 0; i < inputs.length; i++) {
     
     let field = inputs[i];
-    console.log(field);
+    // console.log(field);
 
     field.addEventListener('input', (e) => {
 
@@ -32,3 +32,31 @@ toggleListChoix.addEventListener('click', function(){
     multiSelect.classList.toggle('hidden');
 });
 
+
+
+// affichage modale instruction cheque
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const modal = document.getElementById('myModal');
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      // Deselect all other checkboxes
+      checkboxes.forEach((otherCheckbox) => {
+        if (otherCheckbox !== this) {
+          otherCheckbox.checked = false;
+        }
+      });
+      // Show modal if .cheque-option is checked
+      if (this.classList.contains('cheque-option')) {
+        modal.style.display = 'block';
+      } else {
+        modal.style.display = 'none';
+      }
+    } else {
+      // If the checkbox is unchecked, hide the modal
+      modal.style.display = 'none';
+    }
+  });
+});
